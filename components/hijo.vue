457b705b0aaf09ca21afdb923f88868b851bc432
emirand <you@jaumebalmes.net>
2023-01-27 13:04:10 +0100

    ToDO

--- components/hijo.vue
+++ components/hijo.vue
@@ -1,43 +1,71 @@
 <template>
-    <div>
-        <h1>{{ saludo }}</h1>
-        <button @click="accion">Acción</button>
-    </div>
+  <div>
+    <li>
+      <input type="checkbox" v-model="item.checked" />
+      <span :class="item.checked ? 'strikeout' : null">{{ item.text }}</span>
+      <button @click="hideItem(item)">x</button>
+    </li>
+    <h1>{{ saludo }}</h1>
+    <!-- <button @click="accion">Acción</button> -->
+  </div>
 </template>
 <script>
 export default {
-  props: ['name'],
+  props: ["tarea", "nueva"],
   data() {
     return {
-      // saludo: "Hola ",
+      items: [
+        {
+          text: "Tomates",
+          checked: false,
+          deleted: false,
+        },
+        {
+          text: "Judias",
+          checked: false,
+          deleted: false,
+        },
+        {
+          text: "Manzanas",
+          checked: false,
+          deleted: false,
+        },
+      ],
     };
   },
-  mounted(){
-
-  },
-  destroyed() {
-    
-  },
-  computed:{
-    saludo(){
-      return 'hola ' + this.name
-    }
-  },
-  watch:{
-
+  computed: {
+    saludo() {
+      return this.tarea;
+    },
   },
   methods: {
-    accion(){
-      this.$emit("foo", 'Pedro')
-    }
+    // accion() {
+    //   this.$emit("foo", "Pedro");
+    // },
+
+    addItem() {
+      if (!this.nueva) {
+        alert("añade tarea");
+      } else {
+        this.items.push({
+          text: this.nueva,
+          checked: false,
+          deleted: false,
+        });
+      }
+    },
+
+    hideItem(item) {
+      item.deleted = true;
+    },
+
+    muestraElementosPapelera() {
+      this.items.forEach((element) => {
+        if (element.deleted == true) {
+          element.deleted = false;
+        }
+      });
+    },
   },
 };
 </script>
-
-
-<style scoped>
-h1{
-    color: red;
-}
-
-</style>
--- components/padre.vue
+++ components/padre.vue
@@ -1,20 +1,82 @@
 <template>
-  <div>
-    <input type="text" v-model="nombre" />
-    <Hijo :name="nombre" @foo="handleFoo"></Hijo>
+  <div class="body">
+    <header>{{ title }}</header>
+    <div>
+      <p>{{ message }}</p>
+
+      <input
+        v-model="nueva"
+        v-on:keypress.enter="addItem"
+        placeholder="Agrega elementos a la lista"
+      />
+      <button @click="addItem">Agrega a la lista</button> 
+      <button @click="muestraElementosPapelera">
+        Ver elementos de la papelera
+      </button>
+      <pre>{{ nueva }}</pre>
+    </div>
+
+    <!-- <Hijo :tarea="nueva" @foo="handleFoo"></Hijo> -->
   </div>
 </template>
+  
 <script>
 export default {
   data() {
     return {
-      nombre: "Juan",
+      title: "To Do",
+      message: "Crea una lista de tareas",
+      nueva: "",
+      items: [
+        {
+          text: "Tomates",
+          checked: false,
+          deleted: false,
+        },
+        {
+          text: "Judias",
+          checked: false,
+          deleted: false,
+        },
+        {
+          text: "Manzanas",
+          checked: false,
+          deleted: false,
+        },
+      ],
     };
   },
-  methods:{
-    handleFoo(data){
-        this.nombre = data
-    }
-  }
+  methods: {
+    handleFoo(data) {
+      this.nueva = data;
+    },
+    addItem() {
+      if (!this.nueva) {
+        alert("añade tarea");
+      } else {
+        this.items.push({
+          text: this.nueva,
+          checked: false,
+          deleted: false,
+        });
+      }
+    },
+  },
 };
 </script>
+
+<style scoped>
+.body {
+  padding: 2em;
+  font-family: sans-serif;
+  text-align: center;
+}
+header {
+  display: block;
+  border-radius: 8px 8px 0 0;
+  background: #434c5e;
+  padding: 10px;
+  color: white;
+  font-size: 1.5em;
+}
+</style>
